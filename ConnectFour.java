1612ab112fdb718ae2a94a0150faac933dd0102e
mattrindfleisch <62218297+mattrindfleisch@users.noreply.github.com>
2020-03-28 20:00:07 -0400

    Add files via upload

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,23 +1,39 @@
-import java.util.*;
+import java.util.Scanner;
 public class ConnectFour
 {
 	final public static char[][] board = new char[6][7];
 		
 	public static void main(String[] args)
 	{
+		int column = 0;
+		char color = 0;
+		boolean winner = false;
+		int playAgain = 1;
 		Scanner input = new Scanner(System.in);
-
-		printBoard(board);
-		
-		//This for loop initializes the array
-		for (int row = 0; row < board.length; row++)
+		do 
 		{
-			for (int col = 0; col < board[0].length; col++)
+			//This for loop initializes the array
+			for (int row = 0; row < board.length; row++)
 			{
-				board[row][col] = ' ';
+				for (int col = 0; col < board[0].length; col++)
+				{
+					board[row][col] = ' ';
+				}
 			}
+			
+			dropDisk(board,column,color);
+			winner = isWinner(board,color);
+		} while (winner == false && playAgain == 1);
+		
+		if (winner == true)
+		{
+			System.out.println("New Game? 0 for NO, 1 for YES");
+			playAgain = 0;
+			playAgain = input.nextInt();
 		}
 	}
+	
+	
 	private static void printBoard(char[][] board)
 	{
 		for(int i = board.length - 1; i >= 0; i--)
@@ -36,71 +52,76 @@ public class ConnectFour
 		int turn = 1;
 		color = 'R';
 		int play = 0;
-		boolean validPlay = false;
+		//boolean validPlay = false;
 		Scanner input = new Scanner(System.in);
 		boolean winner = false;
-		
 		//Checks to see if the play is valid before taking the user input into the next loop.
 		while(winner == false && turn <= 42)
 		{
-			while (validPlay == false)
+			boolean validPlay = false;
+			
+			//This loop calls in the columnIsFull method. If the column is determined to be invalid or full, validPlay will be false, and thus this loop will continue.
+			do
 			{
+				printBoard(board);
+				//play is a variable that represents a user input of which column they would like to drop the disk into.
 				play = input.nextInt();
 				validPlay = columnisFull(board,play);
 			
-			}
-			
-		
-			for (int row = board.length - 1; row >= 0; row--)
-			{
-				if (board[row][play] == ' ')
+			} while (validPlay == false);
+				
+				//This for loop is designed to drop 
+				for (int row = board.length - 6; row >= 0; row++)
 				{
-					board[row][play] = color;
-					break;
+					if (board[row][play] == ' ')
+					{
+						board[row][play] = color;
+						break;
+					}
 				}
-			}
-		
-			//Switches player's turn. If the disk that was just dropped was red, then the next turn is yellow, and vice versa.
-			if (color == 'R')
-			{
-				color = 'Y';
-			}
 			
-			else if (color == 'Y')
-			{
-				color = 'R';
-			}
-			
-			//Calls the isWinner method to determine if there is a winner. If a winner is found, then the game stops.
-			winner = isWinner(board, color);
-			
-			//Increments the turn
-			turn++;
+				//Switches player's turn. If the disk that was just dropped was red, then the next turn is yellow, and vice versa.
+				if (color == 'R')
+				{
+					color = 'Y';
+				}
+				
+				else if (color == 'Y')
+				{
+					color = 'R';
+				}
+				
+				//Calls the isWinner method to determine if there is a winner. If a winner is found, then the game stops.
+				winner = isWinner(board, color);
+				
+				//Increments the turn
+				turn++;
+				
+				if (turn == 43)
+				{
+					System.out.println("Tie Game");
+				}
+				
+	
 		}
-		
+		//If a player meets one of the win conditions, then the board 
 		if (winner == true)
 		{
 			if (color == 'R')
 			{
-				System.out.println("Red player wins");
-				break;
+				printBoard(board);
+				System.out.println("Yellow Player Wins");
 			}
-			
+				
 			else if (color == 'Y')
 			{
-				System.out.println("Yellow player wins");
-				break;
+				printBoard(board);
+				System.out.println("Red player wins");	
 			}
-		}
-		
-		if (turn == 43)
-		{
-			System.out.println("Tie game");
-			break;
 		}
 	}
 	
-	private static boolean isWinner(char[][] board)
+	private static boolean isWinner(char[][] board, char color)
 	{
 		//Checks for a winner with a four-across win condition
 		for (int row = 0; row < board.length; row++)
@@ -109,7 +130,10 @@ public class ConnectFour
 			{
 				if (board[row][col] == board[row][col + 1] && board[row][col + 1] == board[row][col + 2] && board[row][col + 2] == board[row][col +3])
 				{
-					return true;
+					if (board[row][col] == 'R' || board[row][col] == 'Y')
+					{
+						return true;
+					}
 				}
 			}
 		}
@@ -120,30 +144,40 @@ public class ConnectFour
 			{
 				if (board[row][col] == board[row + 1][col] && board[row + 1][col] == board[row + 2][col] && board[row + 2][col] == board[row + 3][col])
 				{
-					return true;
+					if (board[row][col] == 'R' || board[row][col] == 'Y')
+					{
+						return true;
+					}
 				}
 			}
 		}
-		//Checks for a backwards diagonal win condition
+		
+		//Checks for a forwards diagonal win condition
 		for (int row = 0; row < board.length - 3; row++)
 		{
 			for (int col = 0; col < board.length - 3; col++)
 			{
 				if (board[row][col] == board[row + 1][col + 1] && board[row][col] == board[row + 2][col + 2] && board[row][col] == board[row + 3][col + 3])
 				{
-					return true;
+					if (board[row][col] == 'R' || board[row][col] == 'Y')
+					{
+						return true;
+					}
 				}
 			}
 		}
 		
-		//Checks for a forwards diagonal win condition
-		for (int row = 0; row < board.length; row++)
+		//Checks for a backwards diagonal win condition
+		for (int row = 3; row < board.length; row++)
 		{
 			for (int col = 0; col < board[0].length - 3; col++)
 			{
 				if (board[row][col] == board[row - 1][col + 1] && board[row][col] == board[row - 2][col + 2] && board[row][col] == board[row - 3][col + 3])
 				{
-					return true;
+					if (board[row][col] == 'R' || board[row][col] == 'Y')
+					{
+						return true;
+					}
 				}
 			}
 		}
@@ -154,12 +188,12 @@ public class ConnectFour
 	{
 		 /* Checks the board and tells the user if the column is either an invalid number or full. If it is invalid, it returns false, otherwise,
 		 it returns true. */
-		if (board[0][col] != ' ' || col < 0 || col > board[0].length)
-		{
-			System.out.println("INVALID OR FULL COLUMN: PLEASE CHOOSE ANOTHER COLUMN");
-			return false;
-		}
-		return true;
+			if (col < 0 || col > board[0].length || board[5][col] != ' ')
+			{
+				System.out.println("INVALID OR FULL COLUMN: PLEASE CHOOSE ANOTHER COLUMN");
+				return false;
+			}
+			return true;
 	}
 }
 
